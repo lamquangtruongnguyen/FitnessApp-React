b874152d1379054eac3a1748648fe33d0ec2eb9a
lamquangtruongnguyen <tnguyen10@my.hpu.edu>
2023-02-20 23:28:55 -1000

    initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import { useState } from "react";
+import { Box } from "@mui/material";
+
+import Exercises from "../components/Exercises";
+import HeroBanner from "../components/HeroBanner";
+import SearchExercises from "../components/SearchExercises";
+
+const Home = () => {
+  const [exercises, setExercises] = useState([]);
+  //Changes in these state reflect in accross the application
+  const [bodyPart, setBodyPart] = useState("all");
+
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        exercises={exercises}
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+      />
+    </Box>
+  );
+};
+
+export default Home;
